360a55d9cc0c3173a07551adb442ff776e61e521
etherealone0 <pranit.mails@gmail.com>
2024-12-08 10:18:45 +0530

    fix: Created a new file to store Gallery images

---
+++ frontend/src/data/GalleryData.js
@@ -0,0 +1,17 @@
+const galleryData = [
+    {
+        original: "img/gall2019.jpg",
+        thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1015/1000/600/",
+        thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1019/1000/600/",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+        
+    },
+]
+
+export default galleryData;
