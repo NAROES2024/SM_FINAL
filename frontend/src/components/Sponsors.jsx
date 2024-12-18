a1179b67e3285973be52f6268fd37c6ba80f8de4
etherealone0 <pranit.mails@gmail.com>
2024-12-18 19:46:44 +0530

    updated spons image and coming soon

--- frontend/src/components/Sponsors.jsx
+++ frontend/src/components/Sponsors.jsx
@@ -23,7 +23,7 @@ function Sponsors() {
           data-wow-delay="0.1s"
         >
           <h5 class="text-primary-gradient fw-medium">Samudramanthan's</h5>
-          <h1 class="">Our Past Sponsors</h1>
+          <h1 class="">Our Sponsors</h1>
           <p class="mb-0">
             Our heartfelt gratitude to our sponsors for their invaluable support
             in making Samudramanthan a great success.
@@ -39,40 +39,40 @@ function Sponsors() {
               <img src="img\Spons\Vedam.png" class="card-img-top" alt="" />
             </div>
           </div>
-          <h5 className="spons_txt my-4">Title Sponsor</h5>
+          <h5 className="spons_txt my-4">Major Sponsor</h5>
         </div>
         
 
-        <div className="row row-cols-1 row-cols-md-1 g-4 my-2 animated slideInLeft">
+        {/* <div className="row row-cols-1 row-cols-md-1 g-4 my-2 animated slideInLeft">
           <div className="spons_width2 col">
             <div className="card h-100">
               <img src="img\Spons\shoft-2.png" class="card-img-top" alt="" />
             </div>
           </div>
           <h5 className="spons_txt my-4">Event Sponsor</h5>
-        </div>
+        </div> */}
         {/* col */}
         <div className="row row-cols-1 row-cols-md-3 g-4 my-2 new animated slideInRight">
           <div className="spons_width3 col">
-            <div className="card">
-              <img src="img\Spons\grse (1).png" class="card-img-top" alt="" />
+            <div className="card spons-img">
+              <img src="img\Spons\grse(2).png" class="card-img-top" alt="" />
             </div>
             <h5 className="spons_txt my-4">Event Sponsor</h5>
           </div>
 
           <div className="spons_width3 col">
-            <div className="card">
-              <img src="img\Spons\shire (1).png" class="card-img-top" alt="" />
+            <div className="card spons-img">
+              <img src="img\Spons\campus-times-square.png" class="card-img-top" alt="" />
             </div>
             <h5 className="spons_txt my-4">Event Sponsor</h5>
           </div>
 
-          <div className="spons_width3 col">
+          {/* <div className="spons_width3 col">
             <div className="card">
               <img src="img\Spons\gs (1).png" class="card-img-top" alt="" />
             </div>
             <h5 className="spons_txt my-4">Event Sponsor</h5>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
